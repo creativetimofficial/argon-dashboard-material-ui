f642446cb6590c288ee0862063241d2aa7f8574c
EINazare <emanuelioannazare@gmail.com>
2021-02-04 15:56:00 +0200

    style: run prettier

--- src/views/Index.js
+++ src/views/Index.js
@@ -1,11 +1,7 @@
 import React from "react";
 
-function Dashboard(){
-  return (
-    <>
-     Hey from Dashboard
-    </>
-  );
+function Dashboard() {
+  return <>Hey from Dashboard</>;
 }
 
 export default Dashboard;
