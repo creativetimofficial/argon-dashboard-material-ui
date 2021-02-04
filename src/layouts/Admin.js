2d10c08b0f60496d023cdaf00e498dd847273692
EINazare <emanuelioannazare@gmail.com>
2021-02-04 15:55:58 +0200

    style: run prettier

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -18,7 +18,7 @@
 import React from "react";
 import { useLocation, Route, Switch, Redirect } from "react-router-dom";
 // material-ui components
-import Container from '@material-ui/core/Container';
+import Container from "@material-ui/core/Container";
 // core components
 import AdminNavbar from "components/Navbars/AdminNavbar.js";
 import AdminFooter from "components/Footers/AdminFooter.js";
@@ -54,10 +54,7 @@ const Admin = (props) => {
 
   const getBrandText = (path) => {
     for (let i = 0; i < routes.length; i++) {
-      if (
-        location.pathname.indexOf(routes[i].layout + routes[i].path) !==
-        -1
-      ) {
+      if (location.pathname.indexOf(routes[i].layout + routes[i].path) !== -1) {
         return routes[i].name;
       }
     }
@@ -76,10 +73,7 @@ const Admin = (props) => {
         }}
       />
       <div className="main-content" ref={mainContent}>
-        <AdminNavbar
-          {...props}
-          brandText={getBrandText(location.pathname)}
-        />
+        <AdminNavbar {...props} brandText={getBrandText(location.pathname)} />
         <Switch>
           {getRoutes(routes)}
           <Redirect from="*" to="/admin/index" />
