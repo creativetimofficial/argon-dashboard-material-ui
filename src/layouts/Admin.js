17bb8868cdfed7b6b527ac95a7f3d0384f5a1c74
EINazare <emanuelioannazare@gmail.com>
2021-02-04 18:16:21 +0200

    refactor: working over the materia-ui examples

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -18,13 +18,22 @@
 import React from "react";
 import { useLocation, Route, Switch, Redirect } from "react-router-dom";
 // material-ui components
+import { flexGrow } from "@material-ui/system";
 import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
 // core components
 import AdminNavbar from "components/Navbars/AdminNavbar.js";
 import AdminFooter from "components/Footers/AdminFooter.js";
 import Sidebar from "components/Sidebar/Sidebar.js";
 
 import routes from "routes.js";
+
+// const styles = {
+//   "flex-grow": {
+//     flexGrow: 1
+//   }
+// }
 
 const Admin = (props) => {
   const mainContent = React.useRef(null);
@@ -33,7 +42,7 @@ const Admin = (props) => {
   React.useEffect(() => {
     document.documentElement.scrollTop = 0;
     document.scrollingElement.scrollTop = 0;
-    mainContent.current.scrollTop = 0;
+    // mainContent.current.scrollTop = 0;
   }, [location]);
 
   const getRoutes = (routes) => {
@@ -63,25 +72,26 @@ const Admin = (props) => {
 
   return (
     <>
-      <Sidebar
-        {...props}
-        routes={routes}
-        logo={{
-          innerLink: "/admin/index",
-          imgSrc: require("../assets/img/brand/argon-react.png").default,
-          imgAlt: "...",
-        }}
-      />
-      <div className="main-content" ref={mainContent}>
-        <AdminNavbar {...props} brandText={getBrandText(location.pathname)} />
-        <Switch>
-          {getRoutes(routes)}
-          <Redirect from="*" to="/admin/index" />
-        </Switch>
-        <Container>
-          <AdminFooter />
-        </Container>
-      </div>
+      <Grid container>
+        <Sidebar
+          routes={routes}
+          logo={{
+            innerLink: "/admin/index",
+            imgSrc: require("../assets/img/brand/argon-react.png").default,
+            imgAlt: "...",
+          }}
+        />
+        <Box flexGrow="1" flexBasis="auto">
+          <AdminNavbar brandText={getBrandText(location.pathname)} />
+          <Switch>
+            {getRoutes(routes)}
+            <Redirect from="*" to="/admin/index" />
+          </Switch>
+          <Container>
+            <AdminFooter />
+          </Container>
+        </Box>
+      </Grid>
     </>
   );
 };
