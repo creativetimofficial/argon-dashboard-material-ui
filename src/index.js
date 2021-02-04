7b14df873186a326448de292aba696200220b5a5
EINazare <emanuelioannazare@gmail.com>
2021-02-04 15:42:30 +0200

    refactor: add nucleo and fontawesome icons

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 import App from "./App";
-import theme from "./theme";
+import theme from "theme.js";
+
+import "assets/plugins/nucleo/css/nucleo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
