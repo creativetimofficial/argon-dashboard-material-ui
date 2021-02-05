4702e4d34bde5045a55e1886ef1d4cbafb9c7e91
EINazare <emanuelioannazare@gmail.com>
2021-02-05 14:45:41 +0200

    feature: add all routes

--- src/routes.js
+++ src/routes.js
@@ -1,60 +1,97 @@
 import Index from "views/Index.js";
-// import Profile from "views/examples/Profile.js";
-// import Maps from "views/examples/Maps.js";
-// import Register from "views/examples/Register.js";
-// import Login from "views/examples/Login.js";
-// import Tables from "views/examples/Tables.js";
-// import Icons from "views/examples/Icons.js";
+import Profile from "views/examples/Profile.js";
+import Maps from "views/examples/Maps.js";
+import Register from "views/examples/Register.js";
+import Login from "views/examples/Login.js";
+import Tables from "views/examples/Tables.js";
+import Icons from "views/examples/Icons.js";
 
 var routes = [
   {
+    href: "#pablo",
+    name: "Upgrade to pro",
+    icon: "ni ni-spaceship",
+    upgradeToPro: true,
+  },
+  {
     path: "/index",
     name: "Dashboard",
-    icon: "ni ni-tv-2 text-primary",
+    icon: "ni ni-tv-2",
+    iconColor: "Primary",
     component: Index,
     layout: "/admin",
   },
-  // {
-  //   path: "/icons",
-  //   name: "Icons",
-  //   icon: "ni ni-planet text-blue",
-  //   component: Icons,
-  //   layout: "/admin",
-  // },
-  // {
-  //   path: "/maps",
-  //   name: "Maps",
-  //   icon: "ni ni-pin-3 text-orange",
-  //   component: Maps,
-  //   layout: "/admin",
-  // },
-  // {
-  //   path: "/user-profile",
-  //   name: "User Profile",
-  //   icon: "ni ni-single-02 text-yellow",
-  //   component: Profile,
-  //   layout: "/admin",
-  // },
-  // {
-  //   path: "/tables",
-  //   name: "Tables",
-  //   icon: "ni ni-bullet-list-67 text-red",
-  //   component: Tables,
-  //   layout: "/admin",
-  // },
-  // {
-  //   path: "/login",
-  //   name: "Login",
-  //   icon: "ni ni-key-25 text-info",
-  //   component: Login,
-  //   layout: "/auth",
-  // },
-  // {
-  //   path: "/register",
-  //   name: "Register",
-  //   icon: "ni ni-circle-08 text-pink",
-  //   component: Register,
-  //   layout: "/auth",
-  // },
+  {
+    path: "/icons",
+    name: "Icons",
+    icon: "ni ni-planet",
+    iconColor: "Primary",
+    component: Icons,
+    layout: "/admin",
+  },
+  {
+    path: "/maps",
+    name: "Maps",
+    icon: "ni ni-pin-3",
+    iconColor: "Warning",
+    component: Maps,
+    layout: "/admin",
+  },
+  {
+    path: "/user-profile",
+    name: "User Profile",
+    icon: "ni ni-single-02",
+    iconColor: "WarningLight",
+    component: Profile,
+    layout: "/admin",
+  },
+  {
+    path: "/tables",
+    name: "Tables",
+    icon: "ni ni-bullet-list-67",
+    iconColor: "Error",
+    component: Tables,
+    layout: "/admin",
+  },
+  {
+    path: "/login",
+    name: "Login",
+    icon: "ni ni-key-25",
+    iconColor: "Info",
+    component: Login,
+    layout: "/auth",
+  },
+  {
+    path: "/register",
+    name: "Register",
+    icon: "ni ni-circle-08",
+    iconColor: "ErrorLight",
+    component: Register,
+    layout: "/auth",
+  },
+  {
+    divider: true,
+  },
+  {
+    title: "Documentation",
+  },
+  {
+    href:
+      "https://demos.creative-tim.com/argon-dashboard-react/#/documentation/overview",
+    name: "Getting started",
+    icon: "ni ni-spaceship",
+  },
+  {
+    href:
+      "https://demos.creative-tim.com/argon-dashboard-react/#/documentation/overview",
+    name: "Foundation",
+    icon: "ni ni-palette",
+  },
+  {
+    href:
+      "https://demos.creative-tim.com/argon-dashboard-react/#/documentation/overview",
+    name: "Components",
+    icon: "ni ni-ui-04",
+  },
 ];
 export default routes;
