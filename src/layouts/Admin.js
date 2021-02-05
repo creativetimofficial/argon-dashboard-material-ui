5a82d5ea166c4dbf54ef5c4b1d27a7dd80dad986
EINazare <emanuelioannazare@gmail.com>
2021-02-05 17:54:12 +0200

    fix: solve eslint issues

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -19,7 +19,6 @@ import React from "react";
 import { useLocation, Route, Switch, Redirect } from "react-router-dom";
 // material-ui components
 // import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
 // core components
