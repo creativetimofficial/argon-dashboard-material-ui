89de6f8ef0f1d3b98b956f8d2c749a14589a05c1
EINazare <emanuelioannazare@gmail.com>
2021-02-05 17:03:17 +0200

    feature: make normal admin footer look like adr

--- src/components/Footers/AdminFooter.js
+++ src/components/Footers/AdminFooter.js
@@ -1,21 +1,48 @@
 import React from "react";
 
 // material-ui components
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemLink from "@material-ui/core/ListItemText";
+
+const useStyles = makeStyles((theme) => ({
+  listItemRoot: {
+    width: "auto",
+    color: "#8898aa",
+    fontSize: ".875rem",
+  },
+  copyrightWrapper: {
+    color: "#8898aa",
+    fontSize: ".875rem",
+    textAlign: "center",
+    [theme.breakpoints.up("md")]: {
+      textAlign: "left",
+    },
+  },
+  copyrightLink: {
+    fontWeight: "600",
+    marginLeft: ".25rem",
+    color: "#5e72e4",
+    backgroundColor: "initial",
+    textDecoration: "none",
+    "&:hover": {
+      color: "#233dd2",
+    },
+  },
+}));
 
 const Footer = () => {
+  const classes = useStyles();
   return (
-    <footer className="footer">
+    <Box component="footer" width="100%">
       <Grid container spacing={3}>
         <Grid item xl={6}>
-          <div className="copyright text-center text-xl-left text-muted">
+          <div className={classes.copyrightWrapper}>
             © {new Date().getFullYear()}{" "}
             <a
-              className="font-weight-bold ml-1"
+              className={classes.copyrightLink}
               href="https://www.creative-tim.com?ref=adr-admin-footer"
               rel="noopener noreferrer"
               target="_blank"
@@ -25,51 +52,70 @@ const Footer = () => {
           </div>
         </Grid>
 
-        <Grid item xl={6}>
-          <List>
-            <ListItem>
-              <ListItemLink
-                href="https://www.creative-tim.com?ref=adr-admin-footer"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <ListItemText>Creative Tim</ListItemText>
-              </ListItemLink>
+        <Grid
+          item
+          xl={6}
+          component={Box}
+          display="flex"
+          justifyContent="flex-end"
+        >
+          <Box
+            component={List}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <ListItem
+              component="a"
+              href="https://www.creative-tim.com?ref=adr-admin-footer"
+              rel="noopener noreferrer"
+              target="_blank"
+              classes={{
+                root: classes.listItemRoot,
+              }}
+            >
+              Creative Tim
             </ListItem>
 
-            <ListItem>
-              <ListItemLink
-                href="https://www.creative-tim.com/presentation?ref=adr-admin-footer"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <ListItemText>About Us</ListItemText>
-              </ListItemLink>
+            <ListItem
+              component="a"
+              href="https://www.creative-tim.com/presentation?ref=adr-admin-footer"
+              rel="noopener noreferrer"
+              target="_blank"
+              classes={{
+                root: classes.listItemRoot,
+              }}
+            >
+              About Us
             </ListItem>
 
-            <ListItem>
-              <ListItemLink
-                href="http://blog.creative-tim.com?ref=adr-admin-footer"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <ListItemText>Blog</ListItemText>
-              </ListItemLink>
+            <ListItem
+              component="a"
+              href="http://blog.creative-tim.com?ref=adr-admin-footer"
+              rel="noopener noreferrer"
+              target="_blank"
+              classes={{
+                root: classes.listItemRoot,
+              }}
+            >
+              Blog
             </ListItem>
 
-            <ListItem>
-              <ListItemLink
-                href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md?ref=adr-admin-footer"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <ListItemText>MIT License</ListItemText>
-              </ListItemLink>
+            <ListItem
+              component="a"
+              href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md?ref=adr-admin-footer"
+              rel="noopener noreferrer"
+              target="_blank"
+              classes={{
+                root: classes.listItemRoot,
+              }}
+            >
+              MIT License
             </ListItem>
-          </List>
+          </Box>
         </Grid>
       </Grid>
-    </footer>
+    </Box>
   );
 };
 
