49ac178189266ddd0a5b3f0e75f9cd2ebf9415ab
EINazare <emanuelioannazare@gmail.com>
2021-02-08 18:01:31 +0200

    feature: add base code for Login page
    WIP: still have to style the inputs and the checkbox

--- src/views/examples/Login.js
+++ src/views/examples/Login.js
@@ -1,7 +1,185 @@
 import React from "react";
 
+// material-ui components
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Box from "@material-ui/core/Box";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Checkbox from "@material-ui/core/Checkbox";
+import FilledInput from "@material-ui/core/FilledInput";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+// import FormGroup from '@material-ui/core/FormGroup';
+// import FormLabel from '@material-ui/core/FormLabel';
+import Grid from "@material-ui/core/Grid";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import InputLabel from "@material-ui/core/InputLabel";
+// material-ui icons
+import AccountCircle from "@material-ui/icons/AccountCircle";
+
+const useStyles = makeStyles((theme) => ({
+  cardRoot: {
+    boxShadow: "0 0 2rem 0 rgba(136,152,170,.15)!important",
+    border: "0!important",
+    backgroundColor: "#f7fafc",
+  },
+  cardHeader: {
+    backgroundColor: "initial",
+  },
+  buttonImg: {
+    verticalAlign: "middle",
+    borderStyle: "none",
+  },
+  buttonRoot: {
+    backgroundColor: "#fff",
+    color: theme.palette.primary.main,
+    boxShadow: "0 4px 6px rgb(50 50 93 / 11%), 0 1px 3px rgb(0 0 0 / 8%)",
+    borderColor: "#fff!important",
+    "&:hover": {
+      color: "#212529",
+      borderColor: "#fff!important",
+      backgroundColor: "#fff",
+    },
+  },
+  formControlLabelRoot: {
+    position: "relative",
+    display: "flex",
+    minHeight: "1.5rem",
+    paddingLeft: "3rem",
+    WebkitPrintColorAdjust: "exact",
+  },
+  formControlLabelLabel: {
+    cursor: "pointer",
+    fontSize: ".875rem",
+    position: "relative",
+    verticalAlign: "top",
+    display: "inline-block",
+  },
+}));
+
 function Login() {
-  return <>Hey from Login</>;
+  const classes = useStyles();
+  return (
+    <>
+      <Grid item lg={5} md={7}>
+        <Card classes={{ root: classes.cardRoot }}>
+          <Box
+            padding="1.25rem 1.5rem"
+            marginBottom="0"
+            borderBottom="1px solid rgba(0,0,0,.05)"
+            paddingBottom="3rem"
+            borderRadius="calc(.375rem - 1px) calc(.375rem - 1px) 0 0"
+            className={classes.cardHeader}
+          >
+            <Box
+              color="#8898aa"
+              textAlign="center"
+              marginBottom="1rem"
+              marginTop=".5rem"
+              fontSize="1rem"
+            >
+              <Box fontSize="80%" fontWeight="400" component="small">
+                Sign in with
+              </Box>
+            </Box>
+            <Box textAlign="center">
+              <Box
+                component={Button}
+                variant="contained"
+                marginRight=".5rem!important"
+                classes={{ root: classes.buttonRoot }}
+              >
+                <Box component="span" marginRight="4px">
+                  <Box
+                    alt="..."
+                    component="img"
+                    width="20px"
+                    className={classes.buttonImg}
+                    src={require("assets/img/icons/common/github.svg").default}
+                  ></Box>
+                </Box>
+                <Box component="span" marginLeft=".75rem">
+                  Github
+                </Box>
+              </Box>
+              <Button
+                variant="contained"
+                classes={{ root: classes.buttonRoot }}
+              >
+                <Box component="span" marginRight="4px">
+                  <Box
+                    alt="..."
+                    component="img"
+                    width="20px"
+                    className={classes.buttonImg}
+                    src={require("assets/img/icons/common/google.svg").default}
+                  ></Box>
+                </Box>
+                <Box component="span" marginLeft=".75rem">
+                  Google
+                </Box>
+              </Button>
+            </Box>
+          </Box>
+          <CardContent>
+            <Box
+              color="#8898aa"
+              textAlign="center"
+              marginBottom="1rem"
+              marginTop=".5rem"
+              fontSize="1rem"
+            >
+              <Box fontSize="80%" fontWeight="400" component="small">
+                Sign in with
+              </Box>
+            </Box>
+            <FormControl variant="filled">
+              <InputLabel htmlFor="login-page-card-email">Email</InputLabel>
+              <FilledInput
+                id="login-page-card-email"
+                type="email"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <AccountCircle />
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <FormControl variant="filled">
+              <InputLabel htmlFor="login-page-card-password">
+                Password
+              </InputLabel>
+              <FilledInput
+                id="login-page-card-password"
+                type="password"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <AccountCircle />
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <FormControlLabel
+              value="end"
+              control={<Checkbox color="primary" />}
+              label="Remeber me"
+              labelPlacement="end"
+              classes={{
+                root: classes.formControlLabelRoot,
+                label: classes.formControlLabelLabel,
+              }}
+            />
+            <Box textAlign="center" marginTop="1.5rem" marginBottom="1.5rem">
+              <Button color="primary" variant="contained">
+                Sign in
+              </Button>
+            </Box>
+          </CardContent>
+        </Card>
+      </Grid>
+    </>
+  );
 }
 
 export default Login;
