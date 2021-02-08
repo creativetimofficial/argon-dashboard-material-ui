45f91b36422a59781dd35315f841281d6260e04f
EINazare <emanuelioannazare@gmail.com>
2021-02-08 16:13:16 +0200

    feature: add auth layout components

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import "assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import AdminLayout from "layouts/Admin.js";
-// import AuthLayout from "layouts/Auth.js";
+import AuthLayout from "layouts/Auth.js";
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
@@ -20,7 +20,7 @@ ReactDOM.render(
     <BrowserRouter>
       <Switch>
         <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
-        {/* <Route path="/auth" render={(props) => <AuthLayout {...props} />} /> */}
+        <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
         <Redirect from="/" to="/admin/index" />
       </Switch>
     </BrowserRouter>
