9d7334872a87a6af5199770e07ebdce4f0d6c54b
EINazare <emanuelioannazare@gmail.com>
2021-02-10 12:45:47 +0200

    feature: add all card stats

--- src/components/Headers/Header.js
+++ src/components/Headers/Header.js
@@ -18,7 +18,16 @@
 import React from "react";
 // material-ui components
 import { makeStyles } from "@material-ui/core/styles";
-
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+// material-ui icons
+import ArrowDownward from "@material-ui/icons/ArrowDownward";
+import ArrowUpward from "@material-ui/icons/ArrowUpward";
+import InsertChartOutlined from "@material-ui/icons/InsertChartOutlined";
+import GroupAdd from "@material-ui/icons/GroupAdd";
+import EmojiEvents from "@material-ui/icons/EmojiEvents";
+import PieChart from "@material-ui/icons/PieChart";
 // core components
 import CardStats from "components/Cards/CardStats.js";
 
@@ -32,6 +41,12 @@ const useStyles = makeStyles((theme) => ({
       paddingTop: "8rem",
     },
   },
+  containerRoot: {
+    [theme.breakpoints.up("md")]: {
+      paddingLeft: "39px",
+      paddingRight: "39px",
+    },
+  },
 }));
 
 const Header = () => {
@@ -39,7 +54,136 @@ const Header = () => {
   return (
     <>
       <div className={classes.header}>
-        <CardStats />
+        <Container
+          maxWidth={false}
+          component={Box}
+          classes={{ root: classes.containerRoot }}
+        >
+          <div>
+            <Grid container>
+              <Grid item xl={3} lg={6}>
+                <CardStats
+                  subtitle="Traffic"
+                  title="350,897"
+                  icon={InsertChartOutlined}
+                  color="bgError"
+                  footer={
+                    <>
+                      <Box
+                        component="span"
+                        fontSize=".875rem"
+                        color="#2dce89"
+                        marginRight=".5rem"
+                        display="flex"
+                        alignItems="center"
+                      >
+                        <Box
+                          component={ArrowUpward}
+                          width="1.5rem!important"
+                          height="1.5rem!important"
+                        />{" "}
+                        3.48%
+                      </Box>
+                      <Box component="span" whiteSpace="nowrap">
+                        Since last month
+                      </Box>
+                    </>
+                  }
+                />
+              </Grid>
+              <Grid item xl={3} lg={6}>
+                <CardStats
+                  subtitle="New users"
+                  title="2,356"
+                  icon={PieChart}
+                  color="bgWarning"
+                  footer={
+                    <>
+                      <Box
+                        component="span"
+                        fontSize=".875rem"
+                        color="#f5365c"
+                        marginRight=".5rem"
+                        display="flex"
+                        alignItems="center"
+                      >
+                        <Box
+                          component={ArrowDownward}
+                          width="1.5rem!important"
+                          height="1.5rem!important"
+                        />{" "}
+                        3.48%
+                      </Box>
+                      <Box component="span" whiteSpace="nowrap">
+                        Since last week
+                      </Box>
+                    </>
+                  }
+                />
+              </Grid>
+              <Grid item xl={3} lg={6}>
+                <CardStats
+                  subtitle="Sales"
+                  title="924"
+                  icon={GroupAdd}
+                  color="bgWarningLight"
+                  footer={
+                    <>
+                      <Box
+                        component="span"
+                        fontSize=".875rem"
+                        color="#fb6340"
+                        marginRight=".5rem"
+                        display="flex"
+                        alignItems="center"
+                      >
+                        <Box
+                          component={ArrowDownward}
+                          width="1.5rem!important"
+                          height="1.5rem!important"
+                        />{" "}
+                        1.10%
+                      </Box>
+                      <Box component="span" whiteSpace="nowrap">
+                        Since yesterday
+                      </Box>
+                    </>
+                  }
+                />
+              </Grid>
+              <Grid item xl={3} lg={6}>
+                <CardStats
+                  subtitle="Performance"
+                  title="49,65%"
+                  icon={EmojiEvents}
+                  color="bgInfo"
+                  footer={
+                    <>
+                      <Box
+                        component="span"
+                        fontSize=".875rem"
+                        color="#2dce89"
+                        marginRight=".5rem"
+                        display="flex"
+                        alignItems="center"
+                      >
+                        <Box
+                          component={ArrowUpward}
+                          width="1.5rem!important"
+                          height="1.5rem!important"
+                        />{" "}
+                        Since last month%
+                      </Box>
+                      <Box component="span" whiteSpace="nowrap">
+                        Since last month
+                      </Box>
+                    </>
+                  }
+                />
+              </Grid>
+            </Grid>
+          </div>
+        </Container>
       </div>
     </>
   );
