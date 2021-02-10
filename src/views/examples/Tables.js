ab308dd136c627b4d5bd5845cf41f2508789c865
EINazare <emanuelioannazare@gmail.com>
2021-02-10 17:19:09 +0200

    feature: start work on Tables page
    WIP: the first table is WIP and the second one is non-exiting atm

--- src/views/examples/Tables.js
+++ src/views/examples/Tables.js
@@ -1,7 +1,485 @@
 import React from "react";
 
-function Tables() {
-  return <>Hey from Tables</>;
-}
+// material-ui components
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import Container from "@material-ui/core/Container";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+// import Grid from "@material-ui/core/Grid";
+
+// core components
+import Header from "components/Headers/Header.js";
+
+const useStyles = makeStyles((theme) => ({
+  cardRoot: {
+    boxShadow: "0 0 2rem 0 rgba(136,152,170,.15)!important",
+  },
+  cardHeader: {
+    backgroundColor: "initial",
+  },
+  containerRoot: {
+    [theme.breakpoints.up("md")]: {
+      paddingLeft: "39px",
+      paddingRight: "39px",
+    },
+  },
+  tableRoot: {
+    marginBottom: "0!important",
+  },
+  tableCellRoot: {
+    verticalAlign: "middle",
+    paddingLeft: "1.5rem",
+    paddingRight: "1.5rem",
+    borderTop: "0",
+  },
+  tableCellRootHead: {
+    backgroundColor: "#f6f9fc",
+    color: "#8898aa",
+  },
+  tableCellRootBodyHead: {
+    textTransform: "unset!important",
+    fontSize: ".8125rem",
+  },
+  linearProgressRoot: {
+    height: "3px!important",
+    width: "120px!important",
+    margin: "0!important",
+  },
+  bgGradientError: {
+    background: "linear-gradient(87deg,#f5365c,#f56036)!important",
+  },
+  bgGradientSuccess: {
+    background: "linear-gradient(87deg,#2dce89,#2dcecc)!important",
+  },
+  bgGradientPrimary: {
+    background: "linear-gradient(87deg,#5e72e4,#825ee4)!important",
+  },
+  bgGradientInfo: {
+    background: "linear-gradient(87deg,#11cdef,#1171ef)!important",
+  },
+  bgGradientWarning: {
+    background: "linear-gradient(87deg,#fb6340,#fbb140)!important",
+  },
+  bgPrimary: {
+    backgroundColor: theme.palette.primary.main,
+  },
+  bgPrimaryLight: {
+    backgroundColor: theme.palette.primary.light,
+  },
+  bgError: {
+    backgroundColor: theme.palette.error.main,
+  },
+  bgErrorLight: {
+    backgroundColor: theme.palette.error.light,
+  },
+  bgWarning: {
+    backgroundColor: theme.palette.warning.main,
+  },
+  bgWarningLight: {
+    backgroundColor: theme.palette.warning.light,
+  },
+  bgInfo: {
+    backgroundColor: theme.palette.info.main,
+  },
+  bgInfoLight: {
+    backgroundColor: theme.palette.info.light,
+  },
+  bgSuccess: {
+    backgroundColor: theme.palette.success.main,
+  },
+  verticalAlignMiddle: {
+    verticalAlign: "middle",
+  },
+}));
+
+const Tables = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <Header />
+      {/* Page content */}
+      <Container
+        maxWidth={false}
+        component={Box}
+        marginTop="-6rem"
+        classes={{ root: classes.containerRoot }}
+      >
+        <Card classes={{ root: classes.cardRoot }}>
+          <CardHeader
+            className={classes.cardHeader}
+            title="Card Tables"
+            titleTypographyProps={{
+              component: Box,
+              marginBottom: "0!important",
+              variant: "h3",
+            }}
+          ></CardHeader>
+          <TableContainer>
+            <Box
+              component={Table}
+              alignItems="center"
+              marginBottom="0!important"
+            >
+              <TableHead>
+                <TableRow>
+                  <TableCell
+                    classes={{
+                      root:
+                        classes.tableCellRoot + " " + classes.tableCellRootHead,
+                    }}
+                  >
+                    Project
+                  </TableCell>
+                  <TableCell
+                    classes={{
+                      root:
+                        classes.tableCellRoot + " " + classes.tableCellRootHead,
+                    }}
+                  >
+                    Budget
+                  </TableCell>
+                  <TableCell
+                    classes={{
+                      root:
+                        classes.tableCellRoot + " " + classes.tableCellRootHead,
+                    }}
+                  >
+                    Status
+                  </TableCell>
+                  <TableCell
+                    classes={{
+                      root:
+                        classes.tableCellRoot + " " + classes.tableCellRootHead,
+                    }}
+                  >
+                    Users
+                  </TableCell>
+                  <TableCell
+                    classes={{
+                      root:
+                        classes.tableCellRoot + " " + classes.tableCellRootHead,
+                    }}
+                  >
+                    Completion
+                  </TableCell>
+                  <TableCell
+                    classes={{
+                      root:
+                        classes.tableCellRoot + " " + classes.tableCellRootHead,
+                    }}
+                  ></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell
+                    classes={{
+                      root:
+                        classes.tableCellRoot +
+                        " " +
+                        classes.tableCellRootBodyHead,
+                    }}
+                    component="th"
+                    variant="head"
+                    scope="row"
+                  >
+                    Argon Design System
+                  </TableCell>
+                  <TableCell classes={{ root: classes.tableCellRoot }}>
+                    $2,500 USD
+                  </TableCell>
+                  <TableCell classes={{ root: classes.tableCellRoot }}>
+                    <Box paddingTop=".35rem" paddingBottom=".35rem">
+                      <Box
+                        marginRight="10px"
+                        component="i"
+                        width=".375rem"
+                        height=".375rem"
+                        borderRadius="50%"
+                        display="inline-block"
+                        className={
+                          classes.verticalAlignMiddle + " " + classes.bgWarning
+                        }
+                      ></Box>
+                      pending
+                    </Box>
+                  </TableCell>
+                  <TableCell
+                    classes={{ root: classes.tableCellRoot }}
+                  ></TableCell>
+                  <TableCell classes={{ root: classes.tableCellRoot }}>
+                    <Box display="flex" alignItems="center">
+                      <Box component="span" marginRight=".5rem">
+                        60%
+                      </Box>
+                      <Box width="100%">
+                        <LinearProgress
+                          variant="determinate"
+                          value={60}
+                          classes={{
+                            root: classes.linearProgressRoot,
+                            bar: classes.bgGradientError,
+                          }}
+                        />
+                      </Box>
+                    </Box>
+                  </TableCell>
+                  <TableCell
+                    classes={{ root: classes.tableCellRoot }}
+                  ></TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell
+                    classes={{
+                      root:
+                        classes.tableCellRoot +
+                        " " +
+                        classes.tableCellRootBodyHead,
+                    }}
+                    component="th"
+                    variant="head"
+                    scope="row"
+                  >
+                    Angular Now UI Kit PRO
+                  </TableCell>
+                  <TableCell classes={{ root: classes.tableCellRoot }}>
+                    $1,800 USD
+                  </TableCell>
+                  <TableCell classes={{ root: classes.tableCellRoot }}>
+                    <Box paddingTop=".35rem" paddingBottom=".35rem">
+                      <Box
+                        marginRight="10px"
+                        component="i"
+                        width=".375rem"
+                        height=".375rem"
+                        borderRadius="50%"
+                        display="inline-block"
+                        className={
+                          classes.verticalAlignMiddle + " " + classes.bgSuccess
+                        }
+                      ></Box>
+                      completed
+                    </Box>
+                  </TableCell>
+                  <TableCell
+                    classes={{ root: classes.tableCellRoot }}
+                  ></TableCell>
+                  <TableCell classes={{ root: classes.tableCellRoot }}>
+                    <Box display="flex" alignItems="center">
+                      <Box component="span" marginRight=".5rem">
+                        70%
+                      </Box>
+                      <Box width="100%">
+                        <LinearProgress
+                          variant="determinate"
+                          value={60}
+                          classes={{
+                            root: classes.linearProgressRoot,
+                            bar: classes.bgGradientSuccess,
+                          }}
+                        />
+                      </Box>
+                    </Box>
+                  </TableCell>
+                  <TableCell
+                    classes={{ root: classes.tableCellRoot }}
+                  ></TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell
+                    classes={{
+                      root:
+                        classes.tableCellRoot +
+                        " " +
+                        classes.tableCellRootBodyHead,
+                    }}
+                    component="th"
+                    variant="head"
+                    scope="row"
+                  >
+                    Black Dashboard
+                  </TableCell>
+                  <TableCell classes={{ root: classes.tableCellRoot }}>
+                    $3,150 USD
+                  </TableCell>
+                  <TableCell classes={{ root: classes.tableCellRoot }}>
+                    <Box paddingTop=".35rem" paddingBottom=".35rem">
+                      <Box
+                        marginRight="10px"
+                        component="i"
+                        width=".375rem"
+                        height=".375rem"
+                        borderRadius="50%"
+                        display="inline-block"
+                        className={
+                          classes.verticalAlignMiddle + " " + classes.bgError
+                        }
+                      ></Box>
+                      delayed
+                    </Box>
+                  </TableCell>
+                  <TableCell
+                    classes={{ root: classes.tableCellRoot }}
+                  ></TableCell>
+                  <TableCell classes={{ root: classes.tableCellRoot }}>
+                    <Box display="flex" alignItems="center">
+                      <Box component="span" marginRight=".5rem">
+                        80%
+                      </Box>
+                      <Box width="100%">
+                        <LinearProgress
+                          variant="determinate"
+                          value={60}
+                          classes={{
+                            root: classes.linearProgressRoot,
+                            bar: classes.bgGradientPrimary,
+                          }}
+                        />
+                      </Box>
+                    </Box>
+                  </TableCell>
+                  <TableCell
+                    classes={{ root: classes.tableCellRoot }}
+                  ></TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell
+                    classes={{
+                      root:
+                        classes.tableCellRoot +
+                        " " +
+                        classes.tableCellRootBodyHead,
+                    }}
+                    component="th"
+                    variant="head"
+                    scope="row"
+                  >
+                    React Material Dashboard
+                  </TableCell>
+                  <TableCell classes={{ root: classes.tableCellRoot }}>
+                    $4,400 USD
+                  </TableCell>
+                  <TableCell classes={{ root: classes.tableCellRoot }}>
+                    <Box paddingTop=".35rem" paddingBottom=".35rem">
+                      <Box
+                        marginRight="10px"
+                        component="i"
+                        width=".375rem"
+                        height=".375rem"
+                        borderRadius="50%"
+                        display="inline-block"
+                        className={
+                          classes.verticalAlignMiddle + " " + classes.bgInfo
+                        }
+                      ></Box>
+                      on schedule
+                    </Box>
+                  </TableCell>
+                  <TableCell
+                    classes={{ root: classes.tableCellRoot }}
+                  ></TableCell>
+                  <TableCell classes={{ root: classes.tableCellRoot }}>
+                    <Box display="flex" alignItems="center">
+                      <Box component="span" marginRight=".5rem">
+                        75%
+                      </Box>
+                      <Box width="100%">
+                        <LinearProgress
+                          variant="determinate"
+                          value={60}
+                          classes={{
+                            root: classes.linearProgressRoot,
+                            bar: classes.bgGradientInfo,
+                          }}
+                        />
+                      </Box>
+                    </Box>
+                  </TableCell>
+
+                  <TableCell
+                    classes={{ root: classes.tableCellRoot }}
+                  ></TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell
+                    classes={{
+                      root:
+                        classes.tableCellRoot +
+                        " " +
+                        classes.tableCellRootBodyHead,
+                    }}
+                    component="th"
+                    variant="head"
+                    scope="row"
+                  >
+                    Vue Paper UI Kit PRO
+                  </TableCell>
+                  <TableCell
+                    classes={{
+                      root: classes.tableCellRoot,
+                    }}
+                  >
+                    $2,200 USD
+                  </TableCell>
+                  <TableCell classes={{ root: classes.tableCellRoot }}>
+                    <Box paddingTop=".35rem" paddingBottom=".35rem">
+                      <Box
+                        marginRight="10px"
+                        component="i"
+                        width=".375rem"
+                        height=".375rem"
+                        borderRadius="50%"
+                        display="inline-block"
+                        className={
+                          classes.verticalAlignMiddle + " " + classes.bgSuccess
+                        }
+                      ></Box>
+                      completed
+                    </Box>
+                  </TableCell>
+                  <TableCell
+                    classes={{ root: classes.tableCellRoot }}
+                  ></TableCell>
+                  <TableCell
+                    classes={{
+                      root: classes.tableCellRoot,
+                    }}
+                  >
+                    <Box display="flex" alignItems="center">
+                      <Box component="span" marginRight=".5rem">
+                        30%
+                      </Box>
+                      <Box width="100%">
+                        <LinearProgress
+                          variant="determinate"
+                          value={60}
+                          classes={{
+                            root: classes.linearProgressRoot,
+                            bar: classes.bgGradientWarning,
+                          }}
+                        />
+                      </Box>
+                    </Box>
+                  </TableCell>
+                  <TableCell
+                    classes={{ root: classes.tableCellRoot }}
+                  ></TableCell>
+                </TableRow>
+              </TableBody>
+            </Box>
+          </TableContainer>
+        </Card>
+      </Container>
+    </>
+  );
+};
 
 export default Tables;
