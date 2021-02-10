db76682780a3d104e600ce53f7599ea946dbdb67
EINazare <emanuelioannazare@gmail.com>
2021-02-10 12:45:38 +0200

    feature: add dynamic code for card stats

--- src/components/Cards/CardStats.js
+++ src/components/Cards/CardStats.js
@@ -1,14 +1,12 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 // material-ui components
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Grid from "@material-ui/core/Grid";
-// material-ui icons
-import ArrowUpward from "@material-ui/icons/ArrowUpward";
-import InsertChartOutlined from "@material-ui/icons/InsertChartOutlined";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
   cardRoot: {
@@ -18,7 +16,10 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   cardContentRoot: {
-    padding: "1rem 1.5rem",
+    padding: "1rem 1.5rem !important",
+  },
+  textUppercase: {
+    textTransform: "uppercase",
   },
   bgPrimary: {
     backgroundColor: theme.palette.primary.main,
@@ -46,29 +47,33 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function CardStats() {
+function CardStats({ subtitle, title, footer, icon, color }) {
   const classes = useStyles();
+  console.log(typeof icon);
   return (
     <>
-      <Card classes={{ root: classes.cardRoot }}>
+      <Card classes={{ root: classes.cardRoot }} elevation={0}>
         <CardContent classes={{ root: classes.cardContentRoot }}>
           <Grid container component={Box} justifyContent="space-between">
             <Grid item xs="auto">
               <Box
-                component="h5"
-                color="#8898aa"
-                marginBottom="0"
-                marginTop="0"
+                component={Typography}
+                variant="h5"
+                color="#8898aa!important"
+                marginBottom="0!important"
+                marginTop="0!important"
+                className={classes.textUppercase}
               >
-                Traffic
+                {subtitle}
               </Box>
               <Box
-                component="h2"
-                fontWeight="600"
-                marginBottom="0"
-                marginTop="0"
+                component={Typography}
+                variant="h2"
+                fontWeight="600!important"
+                marginBottom="0!important"
+                marginTop="0!important"
               >
-                350,897
+                {title}
               </Box>
             </Grid>
             <Grid item xs={"auto"}>
@@ -83,39 +88,65 @@ function CardStats() {
                 borderRadius="50%"
                 boxShadow="0 0 2rem 0 rgba(136,152,170,.15)"
                 color="#fff"
-                className={classes.bgError}
+                className={classes[color]}
               >
-                <Box
-                  component={InsertChartOutlined}
-                  width="1.25rem!important"
-                  height="1.25rem!important"
-                />
+                {icon && typeof icon === "object" ? (
+                  <Box
+                    component={icon}
+                    width="1.5rem!important"
+                    height="1.5rem!important"
+                  />
+                ) : null}
+                {icon && typeof icon === "string" ? (
+                  <Box component="i" fontSize="1.25rem" className={icon} />
+                ) : null}
               </Box>
             </Grid>
           </Grid>
-          <Box
-            component="p"
-            fontSize=".875rem"
-            color="#8898aa"
-            marginTop="1rem"
-            marginBottom="0"
-          >
+          {footer ? (
             <Box
-              component="span"
+              component="p"
               fontSize=".875rem"
-              color="#2dce89"
-              marginRight=".5rem"
+              color="#8898aa"
+              marginTop="1rem"
+              marginBottom="0"
+              display="flex"
+              alignItems="center"
             >
-              <ArrowUpward /> 3.48%
+              {footer}
             </Box>
-            <Box component="span" whiteSpace="nowrap">
-              Since last month
-            </Box>
-          </Box>
+          ) : null}
         </CardContent>
       </Card>
     </>
   );
 }
+
+CardStats.defaultProps = {
+  color: "bgPrimaryLight",
+};
+
+CardStats.propTypes = {
+  subtitle: PropTypes.string,
+  title: PropTypes.string,
+  footer: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  icon: PropTypes.oneOfType([
+    // i.e. an icon name from Nucleo Icons - e.g. ni ni-atom
+    // // or an icon name from Font Awesome - e.g. fa fa-heart
+    PropTypes.string,
+    // i.e. a component from @material-ui/icons
+    PropTypes.object,
+  ]),
+  color: PropTypes.oneOf([
+    "bgPrimary",
+    "bgPrimaryLight",
+    "bgError",
+    "bgErrorLight",
+    "bgWarning",
+    "bgWarningLight",
+    "bgInfo",
+    "bgInfoLight",
+  ]),
+};
 
 export default CardStats;
