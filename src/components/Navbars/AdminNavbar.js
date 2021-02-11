4cb3bf3545ec3eac9c1b68eae1e71d4ed662c760
EINazare <emanuelioannazare@gmail.com>
2021-02-11 18:24:59 +0200

    wip: working on responsive admin layout

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -1,62 +1,30 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
-import Avatar from "@material-ui/core/Avatar";
 import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
-import Divider from "@material-ui/core/Divider";
-import Hidden from "@material-ui/core/Hidden";
-import IconButton from "@material-ui/core/IconButton";
 import InputBase from "@material-ui/core/InputBase";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 // material-ui icons
-import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
-import Person from "@material-ui/icons/Person";
-import Settings from "@material-ui/icons/Settings";
-import EventNote from "@material-ui/icons/EventNote";
-import LiveHelp from "@material-ui/icons/LiveHelp";
-import DirectionsRun from "@material-ui/icons/DirectionsRun";
+
+// core components
+import NavbarDropdown from "components/Dropdowns/NavbarDropdown.js";
 
 const useStyles = makeStyles((theme) => ({
-  buttonRoot: {
-    padding: ".25rem 0 .25rem 1rem!important",
-    border: "0!important",
-  },
-  buttonLabel: {
-    fontSize: ".875rem",
-    fontWeight: "600",
-    color: "hsla(0,0%,100%,.95)",
-    textTransform: "capitalize",
-    display: "flex",
-    alignItems: "center",
-  },
-  avatarRoot: {
-    width: "36px",
-    height: "36px",
-    marginRight: "0.5rem",
-  },
-  dividerRoot: {
-    height: "0",
-    margin: ".5rem 0",
-    overflow: "hidden",
-    borderTop: "1px solid #e9ecef",
-  },
-  menuTitle: {
-    margin: "0!important",
-    textTransform: "uppercase",
-    display: "block",
-    padding: ".5rem 1rem",
-    whiteSpace: "nowrap",
+  appBarRoot: {
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
+    },
   },
   brandTitle: {
     textTransform: "uppercase",
     margin: "0",
     color: "#fff",
+    [theme.breakpoints.down("md")]: {
+      display: "none",
+    },
   },
   searchBox: {
     borderColor: "hsla(0,0%,100%,.6)",
@@ -89,113 +57,14 @@ const useStyles = makeStyles((theme) => ({
 
 export default function AdminNavbar({ brandText }) {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const isMenuOpen = Boolean(anchorEl);
-
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
-
-  const menuId = "primary-search-account-menu";
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <Typography
-        variant="h6"
-        component="h6"
-        classes={{ root: classes.menuTitle }}
-      >
-        Welcome!
-      </Typography>
-      <Box
-        display="flex!important"
-        alignItems="center!important"
-        component={MenuItem}
-        onClick={handleMenuClose}
-      >
-        <Box
-          component={Person}
-          width="1.25rem!important"
-          height="1.25rem!important"
-          marginRight="1rem"
-        />
-        <span>My profile</span>
-      </Box>
-      <Box
-        display="flex!important"
-        alignItems="center!important"
-        component={MenuItem}
-        onClick={handleMenuClose}
-      >
-        <Box
-          component={Settings}
-          width="1.25rem!important"
-          height="1.25rem!important"
-          marginRight="1rem"
-        />
-        <span>Settings</span>
-      </Box>
-      <Box
-        display="flex!important"
-        alignItems="center!important"
-        component={MenuItem}
-        onClick={handleMenuClose}
-      >
-        <Box
-          component={EventNote}
-          width="1.25rem!important"
-          height="1.25rem!important"
-          marginRight="1rem"
-        />
-        <span>Activity</span>
-      </Box>
-      <Box
-        display="flex!important"
-        alignItems="center!important"
-        component={MenuItem}
-        onClick={handleMenuClose}
-      >
-        <Box
-          component={LiveHelp}
-          width="1.25rem!important"
-          height="1.25rem!important"
-          marginRight="1rem"
-        />
-        <span>Support</span>
-      </Box>
-      <Divider component="div" classes={{ root: classes.dividerRoot }} />
-      <Box
-        display="flex!important"
-        alignItems="center!important"
-        component={MenuItem}
-        onClick={handleMenuClose}
-      >
-        <Box
-          component={DirectionsRun}
-          width="1.25rem!important"
-          height="1.25rem!important"
-          marginRight="1rem"
-        />
-        <span>Logout</span>
-      </Box>
-    </Menu>
-  );
-
   return (
     <>
-      <AppBar position="absolute" color="transparent" elevation={0}>
+      <AppBar
+        position="absolute"
+        color="transparent"
+        elevation={0}
+        classes={{ root: classes.appBarRoot }}
+      >
         <Toolbar disableGutters>
           <Container
             maxWidth={false}
@@ -210,15 +79,6 @@ export default function AdminNavbar({ brandText }) {
               marginTop="0.5rem"
             >
               <div>
-                <Hidden mdUp>
-                  <IconButton
-                    edge="start"
-                    color="inherit"
-                    aria-label="open drawer"
-                  >
-                    <MenuIcon />
-                  </IconButton>
-                </Hidden>
                 <Typography
                   className={classes.brandTitle}
                   variant="h4"
@@ -245,33 +105,12 @@ export default function AdminNavbar({ brandText }) {
                     }}
                   />
                 </Box>
-                <Button
-                  edge="end"
-                  aria-label="account of current user"
-                  aria-controls={menuId}
-                  aria-haspopup="true"
-                  onClick={handleProfileMenuOpen}
-                  color="inherit"
-                  classes={{
-                    label: classes.buttonLabel,
-                    root: classes.buttonRoot,
-                  }}
-                >
-                  <Avatar
-                    alt="..."
-                    src={require("assets/img/theme/team-4-800x800.jpg").default}
-                    classes={{
-                      root: classes.avatarRoot,
-                    }}
-                  />
-                  Jessica Jones
-                </Button>
+                <NavbarDropdown />
               </Box>
             </Box>
           </Container>
         </Toolbar>
       </AppBar>
-      {renderMenu}
     </>
   );
 }
