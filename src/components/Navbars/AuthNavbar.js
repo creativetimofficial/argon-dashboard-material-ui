354d58177c80bb2e66e2e3ef833c16f5814c4df6
EINazare <emanuelioannazare@gmail.com>
2021-02-12 12:45:25 +0200

    fix: solve responsive issues

--- src/components/Navbars/AuthNavbar.js
+++ src/components/Navbars/AuthNavbar.js
@@ -5,15 +5,18 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
+import Divider from "@material-ui/core/Divider";
 import Hidden from "@material-ui/core/Hidden";
 import IconButton from "@material-ui/core/IconButton";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Toolbar from "@material-ui/core/Toolbar";
+import Menu from "@material-ui/core/Menu";
 // material-ui icons
 import MenuIcon from "@material-ui/icons/Menu";
 import Dashboard from "@material-ui/icons/Dashboard";
 import AccountCircle from "@material-ui/icons/AccountCircle";
+import Clear from "@material-ui/icons/Clear";
 import VpnKey from "@material-ui/icons/VpnKey";
 import Person from "@material-ui/icons/Person";
 
@@ -21,11 +24,9 @@ const useStyles = makeStyles((theme) => ({
   listItemRoot: {
     display: "flex",
     alignItems: "center",
-    justifyContent: "center",
-    color: "hsla(0,0%,100%,.95)",
     fontSize: "1rem",
-    paddingLeft: ".5rem",
-    paddingRight: ".5rem",
+    paddingLeft: "1.25rem",
+    paddingRight: "1.25rem",
     paddingTop: "1rem",
     paddingBottom: "1rem",
     transition: "all .15s linear",
@@ -33,22 +34,123 @@ const useStyles = makeStyles((theme) => ({
     "& i": {
       marginRight: "0.25rem",
     },
-    "&:hover": {
-      color: "hsla(0,0%,100%,.65)",
-    },
     [theme.breakpoints.up("md")]: {
       marginRight: ".5rem",
+      paddingLeft: ".5rem",
+      paddingRight: ".5rem",
+      color: "hsla(0,0%,100%,.95)",
+      justifyContent: "center",
+      "&:hover": {
+        color: "hsla(0,0%,100%,.65)",
+      },
     },
   },
   headerImg: {
     verticalAlign: "middle",
     borderStyle: "none",
   },
+  menuPaper: {
+    width: "calc(100% - 2rem)",
+  },
+  outlineNone: {
+    outline: "none!important",
+  },
+  flexDirectionColumn: {
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
+    },
+  },
 }));
 
 export default function AuthNavbar() {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const menuId = "responsive-menu-id";
+  const ListObject = (
+    <Box
+      display="flex"
+      alignItems="center"
+      width="auto"
+      component={List}
+      className={classes.flexDirectionColumn}
+    >
+      <ListItem
+        component={Link}
+        to="/admin/dashboard"
+        onClick={handleMenuClose}
+        classes={{
+          root: classes.listItemRoot,
+        }}
+      >
+        <Box
+          component={Dashboard}
+          width="1.25rem!important"
+          height="1.25rem!important"
+          marginRight=".5rem!important"
+        />
+        Dashboard
+      </ListItem>
+      <ListItem
+        component={Link}
+        to="/auth/register"
+        onClick={handleMenuClose}
+        classes={{
+          root: classes.listItemRoot,
+        }}
+      >
+        <Box
+          component={AccountCircle}
+          width="1.25rem!important"
+          height="1.25rem!important"
+          marginRight=".5rem!important"
+        />
+        Register
+      </ListItem>
+      <ListItem
+        component={Link}
+        to="/auth/login"
+        onClick={handleMenuClose}
+        classes={{
+          root: classes.listItemRoot,
+        }}
+      >
+        <Box
+          component={VpnKey}
+          width="1.25rem!important"
+          height="1.25rem!important"
+          marginRight=".5rem!important"
+        />
+        Login
+      </ListItem>
+      <ListItem
+        component={Link}
+        to="/admin/user-profile"
+        onClick={handleMenuClose}
+        classes={{
+          root: classes.listItemRoot,
+        }}
+      >
+        <Box
+          component={Person}
+          width="1.25rem!important"
+          height="1.25rem!important"
+          marginRight=".5rem!important"
+        />
+        Profile
+      </ListItem>
+    </Box>
+  );
   return (
     <>
       <AppBar position="absolute" color="transparent" elevation={0}>
@@ -61,91 +163,80 @@ export default function AuthNavbar() {
             component={Box}
             maxWidth="xl"
           >
-            <div>
-              <Hidden mdUp>
-                <IconButton
-                  edge="start"
-                  color="inherit"
-                  aria-label="open drawer"
-                >
-                  <MenuIcon />
-                </IconButton>
-              </Hidden>
-              <Box
-                alt="..."
-                height="30px"
-                component="img"
-                className={classes.headerImg}
-                src={require("assets/img/brand/argon-react-white.png").default}
-              />
-            </div>
             <Box
-              display="flex"
-              alignItems="center"
-              width="auto"
-              component={List}
-            >
-              <ListItem
-                component={Link}
-                to="/admin/dashboard"
-                classes={{
-                  root: classes.listItemRoot,
-                }}
+              alt="..."
+              height="30px"
+              component="img"
+              className={classes.headerImg}
+              src={require("assets/img/brand/argon-react-white.png").default}
+            />
+            <Hidden mdUp implementation="css">
+              <IconButton
+                edge="start"
+                color="inherit"
+                onClick={handleMenuOpen}
+                aria-controls={menuId}
+                aria-haspopup="true"
               >
                 <Box
-                  component={Dashboard}
-                  width="1.25rem!important"
-                  height="1.25rem!important"
-                  marginRight=".5rem!important"
+                  component={MenuIcon}
+                  color="#fff"
+                  width="2rem!important"
+                  height="2rem!important"
                 />
-                Dashboard
-              </ListItem>
-              <ListItem
-                component={Link}
-                to="/auth/register"
-                classes={{
-                  root: classes.listItemRoot,
-                }}
+              </IconButton>
+              <Menu
+                anchorEl={anchorEl}
+                anchorOrigin={{ vertical: "top", horizontal: "right" }}
+                id={menuId}
+                keepMounted
+                transformOrigin={{ vertical: "top", horizontal: "right" }}
+                open={isMenuOpen}
+                onClose={handleMenuClose}
+                classes={{ paper: classes.menuPaper }}
               >
                 <Box
-                  component={AccountCircle}
-                  width="1.25rem!important"
-                  height="1.25rem!important"
-                  marginRight=".5rem!important"
-                />
-                Register
-              </ListItem>
-              <ListItem
-                component={Link}
-                to="/auth/login"
-                classes={{
-                  root: classes.listItemRoot,
-                }}
-              >
+                  display="flex"
+                  justifyContent="space-between"
+                  alignItems="center"
+                  paddingLeft="1.25rem"
+                  paddingRight="1.25rem"
+                  paddingBottom="1rem"
+                  className={classes.outlineNone}
+                >
+                  <Box
+                    alt="..."
+                    height="36px"
+                    component="img"
+                    className={classes.headerImg}
+                    src={require("assets/img/brand/argon-react.png").default}
+                  />
+                  <IconButton
+                    edge="start"
+                    color="inherit"
+                    onClick={handleMenuClose}
+                    aria-controls={menuId}
+                    aria-haspopup="true"
+                  >
+                    <Box
+                      component={Clear}
+                      width="2rem!important"
+                      height="2rem!important"
+                    />
+                  </IconButton>
+                </Box>
                 <Box
-                  component={VpnKey}
-                  width="1.25rem!important"
-                  height="1.25rem!important"
-                  marginRight=".5rem!important"
+                  component={Divider}
+                  marginBottom="1rem!important"
+                  marginLeft="1.25rem!important"
+                  marginRight="1.25rem!important"
                 />
-                Login
-              </ListItem>
-              <ListItem
-                component={Link}
-                to="/admin/user-profile"
-                classes={{
-                  root: classes.listItemRoot,
-                }}
-              >
-                <Box
-                  component={Person}
-                  width="1.25rem!important"
-                  height="1.25rem!important"
-                  marginRight=".5rem!important"
-                />
-                Profile
-              </ListItem>
-            </Box>
+                {ListObject}
+              </Menu>
+            </Hidden>
+            <Hidden smDown implementation="css">
+              {ListObject}
+            </Hidden>
           </Container>
         </Toolbar>
       </AppBar>
