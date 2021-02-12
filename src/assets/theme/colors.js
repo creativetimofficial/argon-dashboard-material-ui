e98f2e08e10b0c4f09f14cdb6e7ee71a90be7aff
EINazare <emanuelioannazare@gmail.com>
2021-02-12 17:38:37 +0200

    feature: add theme colors

---
+++ src/assets/theme/colors.js
@@ -0,0 +1,59 @@
+const themeColors = {
+  white: {
+    main: "#FFFFFF",
+  },
+  black: {
+    light: "#12263F",
+    main: "#000000",
+  },
+  transparent: {
+    main: "transparent",
+  },
+  gray: {
+    100: "#f6f9fc",
+    200: "#e9ecef",
+    300: "#dee2e6",
+    400: "#ced4da",
+    500: "#adb5bd",
+    600: "#8898aa",
+    700: "#525f7f",
+    800: "#32325d",
+    900: "#212529",
+  },
+  primary: {
+    main: "#5e72e4",
+    dark: "#233dd2",
+  },
+  secondary: {
+    main: "#f7fafc",
+  },
+  warning: {
+    light: "#ffd600",
+    main: "#fb6340",
+  },
+  error: {
+    light: "#f3a4b5",
+    main: "#f5365c",
+  },
+  info: {
+    main: "#11cdef",
+  },
+  background: {
+    default: "#f8f9fe",
+  },
+  text: {
+    primary: "#525f7f",
+  },
+  dark: {
+    tableBorder: "#1f3a68",
+    tableHeadColor: "#4d7bca",
+    tableHeadBgColor: "#1c345d",
+    main: "#172b4d",
+    dark: "#0b1526",
+  },
+  success: {
+    main: "#2dce89",
+  },
+};
+
+export default themeColors;
