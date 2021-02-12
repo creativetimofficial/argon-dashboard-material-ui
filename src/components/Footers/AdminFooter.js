27d65b9f44b3f7dee517fb89e3e017a903ac7e9d
EINazare <emanuelioannazare@gmail.com>
2021-02-12 12:54:25 +0200

    docs: add better comments and order imports

--- src/components/Footers/AdminFooter.js
+++ src/components/Footers/AdminFooter.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-// material-ui components
+// @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+
+// core components
 
 const useStyles = makeStyles((theme) => ({
   listItemRoot: {
