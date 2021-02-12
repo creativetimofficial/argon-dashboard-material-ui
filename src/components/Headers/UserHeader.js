aea7aedacdb532b716bf0925a720698064daa3e0
EINazare <emanuelioannazare@gmail.com>
2021-02-12 12:54:33 +0200

    docs: add better comments and order imports

--- src/components/Headers/UserHeader.js
+++ src/components/Headers/UserHeader.js
@@ -1,29 +1,13 @@
-/*!
-
-=========================================================
-* Argon Dashboard React - v1.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 
-// material-ui components
+// @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+
 // core components
 const useStyles = makeStyles((theme) => ({
   wrapperBox: {
