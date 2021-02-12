8de46b8e31eda0132bbb6f7d0d6d0f271eb9138d
EINazare <emanuelioannazare@gmail.com>
2021-02-12 12:54:57 +0200

    docs: add better comments and order imports

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-// material-ui components
+// @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
 import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardHeader from "@material-ui/core/CardHeader";
@@ -11,14 +11,13 @@ import Checkbox from "@material-ui/core/Checkbox";
 import FilledInput from "@material-ui/core/FilledInput";
 import FormControl from "@material-ui/core/FormControl";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-// import FormGroup from '@material-ui/core/FormGroup';
-// import FormLabel from '@material-ui/core/FormLabel';
 import Grid from "@material-ui/core/Grid";
 import InputAdornment from "@material-ui/core/InputAdornment";
-// material-ui icons
-// import AccountCircle from "@material-ui/icons/AccountCircle";
+// @material-ui/icons components
 import Email from "@material-ui/icons/Email";
 import Lock from "@material-ui/icons/Lock";
+
+// core components
 
 const useStyles = makeStyles((theme) => ({
   cardRoot: {
