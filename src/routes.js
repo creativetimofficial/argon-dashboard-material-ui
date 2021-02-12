62380e9dc89805f6c7bde921aa6a496aac3422e5
EINazare <emanuelioannazare@gmail.com>
2021-02-12 12:56:12 +0200

    docs: order imports

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,21 @@
 import Dashboard from "views/admin/Dashboard.js";
-import Profile from "views/admin/Profile.js";
-import Maps from "views/admin/Maps.js";
-import Register from "views/auth/Register.js";
-import Login from "views/auth/Login.js";
-import Tables from "views/admin/Tables.js";
 import Icons from "views/admin/Icons.js";
+import Login from "views/auth/Login.js";
+import Maps from "views/admin/Maps.js";
+import Profile from "views/admin/Profile.js";
+import Register from "views/auth/Register.js";
+import Tables from "views/admin/Tables.js";
 // @material-ui/icons components
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import Dns from "@material-ui/icons/Dns";
 import FlashOn from "@material-ui/icons/FlashOn";
-import Tv from "@material-ui/icons/Tv";
+import FormatListBulleted from "@material-ui/icons/FormatListBulleted";
 import Grain from "@material-ui/icons/Grain";
 import LocationOn from "@material-ui/icons/LocationOn";
-import Person from "@material-ui/icons/Person";
-import FormatListBulleted from "@material-ui/icons/FormatListBulleted";
-import VpnKey from "@material-ui/icons/VpnKey";
-import AccountCircle from "@material-ui/icons/AccountCircle";
 import Palette from "@material-ui/icons/Palette";
-import Dns from "@material-ui/icons/Dns";
+import Person from "@material-ui/icons/Person";
+import Tv from "@material-ui/icons/Tv";
+import VpnKey from "@material-ui/icons/VpnKey";
 
 var routes = [
   {
