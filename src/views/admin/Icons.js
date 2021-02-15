08eafebaa5a9a4218e7b24409aae3fe9bb2c6353
EINazare <emanuelioannazare@gmail.com>
2021-02-15 15:03:22 +0200

    refactor: move styles to own files

--- src/views/admin/Icons.js
+++ src/views/admin/Icons.js
@@ -16,55 +16,9 @@ import Tooltip from "@material-ui/core/Tooltip";
 // core components
 import Header from "components/Headers/Header.js";
 
-import boxShadows from "assets/theme/box-shadow.js";
+import componentStyles from "assets/theme/views/admin/icons.js";
 
-const useStyles = makeStyles((theme) => ({
-  cardRoot: {
-    boxShadow: boxShadows.boxShadow + "!important",
-  },
-  cardHeader: {
-    backgroundColor: "initial",
-  },
-  button: {
-    verticalAlign: "middle",
-    backgroundColor: theme.palette.gray[200],
-    outline: "none!important",
-    cursor: "pointer",
-    textDecoration: "none",
-    "&:hover": {
-      backgroundColor: theme.palette.white.main,
-      boxShadow: boxShadows.iconsPageButtonBoxShadow,
-    },
-    "& i": {
-      fontSize: "1.5rem",
-      boxSizing: "initial",
-      verticalAlign: "middle",
-      color: theme.palette.gray[800],
-    },
-    "& span": {
-      fontSize: ".875rem",
-      lineHeight: "1.5",
-      display: "inline-block",
-      overflow: "hidden",
-      marginLeft: "16px",
-      verticalAlign: "middle",
-      whiteSpace: "nowrap",
-      textOverflow: "ellipsis",
-      color: theme.palette.gray[800],
-    },
-  },
-  containerRoot: {
-    [theme.breakpoints.up("md")]: {
-      paddingLeft: "39px",
-      paddingRight: "39px",
-    },
-  },
-  iframe: {
-    width: "100%",
-    height: "500px",
-    border: "0",
-  },
-}));
+const useStyles = makeStyles(componentStyles);
 
 const Icons = () => {
   const classes = useStyles();
