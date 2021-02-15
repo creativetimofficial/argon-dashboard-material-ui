dd5998db90a328d3dcfc210d9b96b8f9321a6dbe
EINazare <emanuelioannazare@gmail.com>
2021-02-15 14:45:27 +0200

    refactor: move styles to own files

--- src/components/Footers/AdminFooter.js
+++ src/components/Footers/AdminFooter.js
@@ -8,42 +8,9 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 
 // core components
+import componentStyles from "assets/theme/components/admin-footer.js";
 
-const useStyles = makeStyles((theme) => ({
-  listItemRoot: {
-    width: "auto",
-    color: theme.palette.gray[600],
-    fontSize: ".875rem",
-  },
-  copyrightWrapper: {
-    color: theme.palette.gray[600],
-    fontSize: ".875rem",
-    textAlign: "center",
-    [theme.breakpoints.up("md")]: {
-      textAlign: "left",
-    },
-  },
-  copyrightLink: {
-    fontWeight: "600",
-    marginLeft: ".25rem",
-    color: theme.palette.primary.main,
-    backgroundColor: "initial",
-    textDecoration: "none",
-    "&:hover": {
-      color: theme.palette.primary.dark,
-    },
-  },
-  justifyContentCenter: {
-    [theme.breakpoints.down("lg")]: {
-      justifyContent: "center!important",
-    },
-  },
-  flexDirectionColumn: {
-    [theme.breakpoints.down("sm")]: {
-      flexDirection: "column!important",
-    },
-  },
-}));
+const useStyles = makeStyles(componentStyles);
 
 const Footer = () => {
   const classes = useStyles();
