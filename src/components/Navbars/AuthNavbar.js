1aa5d28c37e5be6ef2acde102c7659c37df1b5e2
EINazare <emanuelioannazare@gmail.com>
2021-02-15 14:45:39 +0200

    refactor: move styles to own files

--- src/components/Navbars/AuthNavbar.js
+++ src/components/Navbars/AuthNavbar.js
@@ -22,48 +22,9 @@ import Person from "@material-ui/icons/Person";
 import VpnKey from "@material-ui/icons/VpnKey";
 
 // core components
+import componentStyles from "assets/theme/components/auth-navbar.js";
 
-const useStyles = makeStyles((theme) => ({
-  listItemRoot: {
-    display: "flex",
-    alignItems: "center",
-    fontSize: "1rem",
-    paddingLeft: "1.25rem",
-    paddingRight: "1.25rem",
-    paddingTop: "1rem",
-    paddingBottom: "1rem",
-    transition: "all .15s linear",
-    fontWeight: "400",
-    "& i": {
-      marginRight: "0.25rem",
-    },
-    [theme.breakpoints.up("md")]: {
-      marginRight: ".5rem",
-      paddingLeft: ".5rem",
-      paddingRight: ".5rem",
-      color: theme.palette.authNavbarLink.dark,
-      justifyContent: "center",
-      "&:hover": {
-        color: theme.palette.authNavbarLink.main,
-      },
-    },
-  },
-  headerImg: {
-    verticalAlign: "middle",
-    borderStyle: "none",
-  },
-  menuPaper: {
-    width: "calc(100% - 2rem)",
-  },
-  outlineNone: {
-    outline: "none!important",
-  },
-  flexDirectionColumn: {
-    [theme.breakpoints.down("md")]: {
-      flexDirection: "column",
-    },
-  },
-}));
+const useStyles = makeStyles(componentStyles);
 
 export default function AuthNavbar() {
   const classes = useStyles();
