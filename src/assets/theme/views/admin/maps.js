fd9247770a11e481e04fa95b4b554b99a83560dc
EINazare <emanuelioannazare@gmail.com>
2021-02-15 15:03:09 +0200

    refactor: move styles to own files

---
+++ src/assets/theme/views/admin/maps.js
@@ -0,0 +1,16 @@
+import boxShadows from "assets/theme/box-shadow.js";
+
+const componentStyles = (theme) => ({
+  cardRoot: {
+    boxShadow: boxShadows.boxShadow + "!important",
+    border: "0!important",
+  },
+  containerRoot: {
+    [theme.breakpoints.up("md")]: {
+      paddingLeft: "39px",
+      paddingRight: "39px",
+    },
+  },
+});
+
+export default componentStyles;
