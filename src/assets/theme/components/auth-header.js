81cca1a2b252aa2b36e53efa037a87353e3a08c9
EINazare <emanuelioannazare@gmail.com>
2021-02-15 14:45:49 +0200

    refactor: move styles to own files

---
+++ src/assets/theme/components/auth-header.js
@@ -0,0 +1,8 @@
+const componentStyles = (theme) => ({
+  header: {
+    background:
+      "linear-gradient(87deg," + theme.palette.info.main + ",#1171ef)",
+  },
+});
+
+export default componentStyles;
