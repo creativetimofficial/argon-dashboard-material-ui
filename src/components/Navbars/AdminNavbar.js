aa0ba66707114cf35465682ff93c6e2e9fad3099
EINazare <emanuelioannazare@gmail.com>
2021-02-15 14:45:37 +0200

    refactor: move styles to own files

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -13,50 +13,9 @@ import SearchIcon from "@material-ui/icons/Search";
 // core components
 import NavbarDropdown from "components/Dropdowns/NavbarDropdown.js";
 
-import boxShadows from "assets/theme/box-shadow.js";
+import componentStyles from "assets/theme/components/admin-navbar.js";
 
-const useStyles = makeStyles((theme) => ({
-  appBarRoot: {
-    [theme.breakpoints.down("sm")]: {
-      display: "none",
-    },
-  },
-  brandTitle: {
-    textTransform: "uppercase",
-    margin: "0",
-    color: theme.palette.white.main,
-    [theme.breakpoints.down("md")]: {
-      display: "none",
-    },
-  },
-  searchBox: {
-    borderColor: theme.palette.adminNavbarSearch.main,
-    borderRadius: "2rem",
-    border: "2px solid",
-    backgroundColor: "initial",
-    boxShadow: boxShadows.inputBoxShadow,
-    transition: "box-shadow .15s ease",
-  },
-  searchIcon: {
-    color: theme.palette.adminNavbarSearch.main,
-    marginRight: "0.5rem",
-    marginLeft: "1rem",
-  },
-  searchInput: {
-    color: theme.palette.adminNavbarSearch.main,
-    width: "270px",
-    backgroundColor: "initial",
-    border: 0,
-    boxShadow: "none",
-    padding: "0",
-  },
-  containerRoot: {
-    [theme.breakpoints.up("md")]: {
-      paddingLeft: "39px",
-      paddingRight: "39px",
-    },
-  },
-}));
+const useStyles = makeStyles(componentStyles);
 
 export default function AdminNavbar({ brandText }) {
   const classes = useStyles();
