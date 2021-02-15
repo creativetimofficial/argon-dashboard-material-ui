2c8ad44e4dcc4153cde6ca529d7b542dcc8d4b13
EINazare <emanuelioannazare@gmail.com>
2021-02-15 14:45:35 +0200

    refactor: move styles to own files

--- src/components/Headers/UserHeader.js
+++ src/components/Headers/UserHeader.js
@@ -10,47 +10,9 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
 // core components
-const useStyles = makeStyles((theme) => ({
-  wrapperBox: {
-    [theme.breakpoints.up("md")]: {
-      paddingTop: "8rem",
-    },
-    backgroundSize: "cover",
-    backgroundPosition: "center top",
-    backgroundImage:
-      "url(" +
-      require("../../assets/img/theme/profile-cover.jpg").default +
-      ")",
-  },
-  overlayBox: {
-    transition: "all .15s ease",
-    opacity: ".9",
-    background:
-      "linear-gradient(87deg," +
-      theme.palette.dark.main +
-      ",#1a174d)!important",
-  },
-  containerRoot: {
-    zIndex: 1,
-    [theme.breakpoints.up("md")]: {
-      paddingLeft: "39px",
-      paddingRight: "39px",
-    },
-  },
-  typographyRootH1: {
-    color: theme.palette.white.main,
-    fontSize: "2.75rem",
-    fontWeight: 600,
-    lineHeight: 1.5,
-  },
-  buttonRoot: {
-    color: theme.palette.white.main,
-    backgroundColor: theme.palette.info.main,
-    "&:hover": {
-      backgroundColor: theme.palette.info.dark,
-    },
-  },
-}));
+import componentStyles from "assets/theme/components/user-header.js";
+
+const useStyles = makeStyles(componentStyles);
 
 const UserHeader = () => {
   const classes = useStyles();
