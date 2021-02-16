5369742a3c84dd2e53858e6ec9f12b9fb862f04d
EINazare <emanuelioannazare@gmail.com>
2021-02-16 18:22:05 +0200

    feature: add scss for react-datetime

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import theme from "assets/theme/theme.js";
 
 import "assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/scss/argon-dashboard-react.scss";
 
 import AdminLayout from "layouts/Admin.js";
 import AuthLayout from "layouts/Auth.js";
