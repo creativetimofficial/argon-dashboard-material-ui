938f9fe720665486e823dfc0af243269a2120add
EINazare <emanuelioannazare@gmail.com>
2021-02-17 11:17:35 +0200

    feature: add colors for snackbars

--- src/assets/theme/colors.js
+++ src/assets/theme/colors.js
@@ -23,20 +23,25 @@ const themeColors = {
   primary: {
     main: "#5e72e4",
     dark: "#233dd2",
+    snackbar: "#7889e8",
   },
   secondary: {
     main: "#f7fafc",
+    snackbar: "#f8fbfc",
   },
   warning: {
     light: "#ffd600",
     main: "#fb6340",
+    snackbar: "#fc7c5f",
   },
   error: {
     light: "#f3a4b5",
     main: "#f5365c",
+    snackbar: "#f75676",
   },
   info: {
     main: "#11cdef",
+    snackbar: "#37d5f2",
   },
   background: {
     default: "#f8f9fe",
@@ -50,9 +55,11 @@ const themeColors = {
     tableHeadBgColor: "#1c345d",
     main: "#172b4d",
     dark: "#0b1526",
+    snackbar: "#3c4d69",
   },
   success: {
     main: "#2dce89",
+    snackbar: "#4fd69c",
   },
 };
 
