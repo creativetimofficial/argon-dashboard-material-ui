a96f059b0381985dce8653629f4c049157de73ce
EINazare <emanuelioannazare@gmail.com>
2021-02-17 15:14:15 +0200

    feature: add colors for badges

--- src/assets/theme/colors.js
+++ src/assets/theme/colors.js
@@ -24,24 +24,37 @@ const themeColors = {
     main: "#5e72e4",
     dark: "#233dd2",
     snackbar: "#7889e8",
+    badgeBg: "#eaecfb",
+    badgeBgHover: "#2a44db",
+    badge: "#2643e9",
   },
   secondary: {
     main: "#f7fafc",
     snackbar: "#f8fbfc",
+    badgeBgHover: "#cadeeb",
   },
   warning: {
     light: "#ffd600",
     main: "#fb6340",
     snackbar: "#fc7c5f",
+    badgeBg: "#fee6e0",
+    badgeBgHover: "#f93305",
+    badge: "#ff3709",
   },
   error: {
     light: "#f3a4b5",
     main: "#f5365c",
     snackbar: "#f75676",
+    badgeBg: "#fdd1da",
+    badgeBgHover: "#e30b36",
+    badge: "#f80031",
   },
   info: {
     main: "#11cdef",
     snackbar: "#37d5f2",
+    badgeBg: "#aaedf9",
+    badgeBgHover: "#0c9cb7",
+    badge: "#03acca",
   },
   background: {
     default: "#f8f9fe",
@@ -56,10 +69,15 @@ const themeColors = {
     main: "#172b4d",
     dark: "#0b1526",
     snackbar: "#3c4d69",
+    badgeBg: "#4172c6",
+    badgeBgHover: "#09111e",
   },
   success: {
     main: "#2dce89",
     snackbar: "#4fd69c",
+    badgeBg: "#b0eed3",
+    badgeBgHover: "#229c68",
+    badge: "#1aae6f",
   },
 };
 
