a2a5571e2e992487f9c612a1b36805c053250e03
EINazare <emanuelioannazare@gmail.com>
2021-02-18 17:55:38 +0200

    feature: add linear-progress styles

---
+++ src/assets/theme/components/linear-progress.js
@@ -0,0 +1,19 @@
+const componentStyles = (theme) => ({
+  linearProgressDefault: {
+    backgroundColor: theme.palette.dark.main,
+  },
+  linearProgressInfo: {
+    backgroundColor: theme.palette.info.main,
+  },
+  linearProgressSuccess: {
+    backgroundColor: theme.palette.success.main,
+  },
+  linearProgressError: {
+    backgroundColor: theme.palette.error.main,
+  },
+  linearProgressWarning: {
+    backgroundColor: theme.palette.warning.main,
+  },
+});
+
+export default componentStyles;
