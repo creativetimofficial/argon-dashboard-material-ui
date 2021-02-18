6fc5d52c257457aff8ebf47bbd4bda2447c194b3
EINazare <emanuelioannazare@gmail.com>
2021-02-18 12:14:28 +0200

    feature: add card img styles

---
+++ src/assets/theme/components/card-img.js
@@ -0,0 +1,12 @@
+const componentStyles = () => ({
+  cardImgTop: {
+    borderTopLeftRadius: "calc(.375rem - 1px)",
+    borderTopRightRadius: "calc(.375rem - 1px)",
+    flexShrink: 0,
+    width: "100%",
+    verticalAlign: "middle",
+    borderStyle: "none",
+  },
+});
+
+export default componentStyles;
