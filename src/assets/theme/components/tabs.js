79145d702457a454a012b4e37ac6c81fb54caa51
EINazare <emanuelioannazare@gmail.com>
2021-02-19 11:51:29 +0200

    feature: add tab style code

---
+++ src/assets/theme/components/tabs.js
@@ -0,0 +1,14 @@
+const componentStyles = () => ({
+  tabCircle: {
+    lineHeight: "60px",
+    width: "60px",
+    height: "60px",
+    padding: 0,
+    textAlign: "center",
+    borderRadius: "50%",
+    flex: "unset",
+    minWidth: "unset!important",
+  },
+});
+
+export default componentStyles;
