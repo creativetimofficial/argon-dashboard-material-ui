467d131997c5a2fccd033a8cf1bed21d3625063b
EINazare <emanuelioannazare@gmail.com>
2021-05-06 13:40:06 +0300

    refactor: move jss to sx prop

--- src/components/Cards/CardStats.js
+++ src/components/Cards/CardStats.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
 import { useTheme } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import Card from "@material-ui/core/Card";
@@ -10,63 +9,105 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
 // core components
-import componentStyles from "assets/theme/components/card-stats.js";
 import boxShadows from "assets/theme/box-shadow.js";
 
-const useStyles = makeStyles(componentStyles);
-
 function CardStats({ subtitle, title, footer, icon, color }) {
-  const classes = useStyles();
   const theme = useTheme();
+  const colorsSx = {
+    bgPrimary: {
+      backgroundColor: theme.palette.primary.main,
+    },
+    bgPrimaryLight: {
+      backgroundColor: theme.palette.primary.light,
+    },
+    bgError: {
+      backgroundColor: theme.palette.error.main,
+    },
+    bgErrorLight: {
+      backgroundColor: theme.palette.error.light,
+    },
+    bgWarning: {
+      backgroundColor: theme.palette.warning.main,
+    },
+    bgWarningLight: {
+      backgroundColor: theme.palette.warning.light,
+    },
+    bgInfo: {
+      backgroundColor: theme.palette.info.main,
+    },
+    bgInfoLight: {
+      backgroundColor: theme.palette.info.light,
+    },
+  };
   return (
     <>
-      <Card classes={{ root: classes.cardRoot }} elevation={0}>
-        <CardContent classes={{ root: classes.cardContentRoot }}>
+      <Card
+        sx={{
+          marginBottom: "1.5rem",
+          [theme.breakpoints.up("xl")]: {
+            marginBottom: 0,
+          },
+        }}
+        elevation={0}
+      >
+        <CardContent sx={{ padding: "1rem 1.5rem !important" }}>
           <Grid container component={Box} justifyContent="space-between">
             <Grid item xs="auto">
               <Box
                 component={Typography}
                 variant="h5"
-                color={theme.palette.gray[600] + "!important"}
-                marginBottom="0!important"
-                marginTop="0!important"
-                className={classes.textUppercase}
+                sx={{
+                  textTransform: "uppercase",
+                  color: theme.palette.gray[600] + "!important",
+                  marginBottom: "0!important",
+                  marginTop: "0!important",
+                }}
               >
                 {subtitle}
               </Box>
               <Box
                 component={Typography}
                 variant="h2"
-                fontWeight="600!important"
-                marginBottom="0!important"
-                marginTop="0!important"
+                sx={{
+                  fontWeight: "600!important",
+                  marginBottom: "0!important",
+                  marginTop: "0!important",
+                }}
               >
                 {title}
               </Box>
             </Grid>
             <Grid item xs={"auto"}>
               <Box
-                width="3rem"
-                height="3rem"
-                padding="12px"
-                textAlign="center"
-                display="inline-flex"
-                alignItems="center"
-                justifyContent="center"
-                borderRadius="50%"
-                boxShadow={boxShadows.boxShadow}
-                color={theme.palette.white.main}
-                className={classes[color]}
+                sx={{
+                  width: "3rem",
+                  height: "3rem",
+                  padding: "12px",
+                  textAlign: "center",
+                  display: "inline-flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  borderRadius: "50%",
+                  boxShadow: boxShadows.boxShadow,
+                  color: theme.palette.white.main,
+                  ...colorsSx[color],
+                }}
               >
                 {icon && typeof icon === "object" ? (
                   <Box
                     component={icon}
-                    width="1.5rem!important"
-                    height="1.5rem!important"
+                    sx={{
+                      width: "1.5rem!important",
+                      height: "1.5rem!important",
+                    }}
                   />
                 ) : null}
                 {icon && typeof icon === "string" ? (
-                  <Box component="i" fontSize="1.25rem" className={icon} />
+                  <Box
+                    component="i"
+                    sx={{ fontSize: "1.25rem" }}
+                    className={icon}
+                  />
                 ) : null}
               </Box>
             </Grid>
@@ -74,13 +115,15 @@ function CardStats({ subtitle, title, footer, icon, color }) {
           {footer ? (
             <Box
               component="p"
-              fontSize=".875rem"
-              color={theme.palette.gray[600]}
-              marginTop="1rem"
-              marginBottom="0"
-              display="flex"
-              alignItems="center"
-              flexWrap="wrap"
+              sx={{
+                fontSize: ".875rem",
+                color: theme.palette.gray[600],
+                marginTop: "1rem",
+                marginBottom: "0",
+                display: "flex",
+                alignItems: "center",
+                flexWrap: "wrap",
+              }}
             >
               {footer}
             </Box>
