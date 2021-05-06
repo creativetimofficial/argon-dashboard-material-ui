2a4b248196ce038fa2d6657e832810478be7a498
EINazare <emanuelioannazare@gmail.com>
2021-05-06 13:40:07 +0300

    refactor: move jss to sx prop

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
@@ -12,58 +12,93 @@ import SearchIcon from "@material-ui/icons/Search";
 
 // core components
 import NavbarDropdown from "components/Dropdowns/NavbarDropdown.js";
-
-import componentStyles from "assets/theme/components/admin-navbar.js";
-
-const useStyles = makeStyles(componentStyles);
+import boxShadows from "assets/theme/box-shadow.js";
 
 export default function AdminNavbar({ brandText }) {
-  const classes = useStyles();
+  const theme = useTheme();
   return (
     <>
       <AppBar
         position="absolute"
         color="transparent"
         elevation={0}
-        classes={{ root: classes.appBarRoot }}
+        sx={{
+          [theme.breakpoints.down("sm")]: {
+            display: "none",
+          },
+        }}
       >
         <Toolbar disableGutters>
           <Container
             maxWidth={false}
             component={Box}
-            classes={{ root: classes.containerRoot }}
+            sx={{
+              [theme.breakpoints.up("md")]: {
+                paddingLeft: "39px",
+                paddingRight: "39px",
+              },
+            }}
           >
             <Box
-              display="flex"
-              justifyContent="space-between"
-              alignItems="center"
-              width="100%"
-              marginTop="0.5rem"
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                width: "100%",
+                marginTop: "0.5rem",
+              }}
             >
               <div>
                 <Typography
-                  className={classes.brandTitle}
+                  sx={{
+                    textTransform: "uppercase",
+                    margin: "0",
+                    color: theme.palette.white.main,
+                    [theme.breakpoints.down("md")]: {
+                      display: "none",
+                    },
+                  }}
                   variant="h4"
                   component="a"
                 >
                   {brandText}
                 </Typography>
               </div>
-              <Box display="flex" alignItems="center" width="auto">
+              <Box
+                sx={{ display: "flex", alignItems: "center", width: "auto" }}
+              >
                 <Box
-                  display="flex"
-                  alignItems="center"
-                  width="auto"
-                  marginRight="1rem"
-                  classes={{
-                    root: classes.searchBox,
+                  sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    width: "auto",
+                    marginRight: "1rem",
+                    borderColor: theme.palette.adminNavbarSearch.main,
+                    borderRadius: "2rem",
+                    border: "2px solid",
+                    backgroundColor: "initial",
+                    boxShadow: boxShadows.inputBoxShadow,
+                    transition: "box-shadow .15s ease",
                   }}
                 >
-                  <SearchIcon className={classes.searchIcon} />
+                  <SearchIcon
+                    sx={{
+                      color: theme.palette.adminNavbarSearch.main,
+                      marginRight: "0.5rem",
+                      marginLeft: "1rem",
+                    }}
+                  />
                   <InputBase
                     placeholder="Search"
-                    classes={{
-                      input: classes.searchInput,
+                    sx={{
+                      "& input": {
+                        color: theme.palette.adminNavbarSearch.main,
+                        width: "270px",
+                        backgroundColor: "initial",
+                        border: 0,
+                        boxShadow: "none",
+                        padding: "0",
+                      },
                     }}
                   />
                 </Box>
